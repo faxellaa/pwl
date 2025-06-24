ee686bb61db2e3deb4a236e1132d544bbeeb9fc5
faxellaa <faxellaa@gmail.com>
2025-06-25 01:23:37 +0700

    style: add middleware for Firebase token verification

---
+++ server/middleware/verifyToken.js
@@ -0,0 +1,21 @@
+import admin from "../config/firebaseAdmin.js";
+
+const verifyFirebaseToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Token tidak ditemukan" });
+  }
+
+  const idToken = authHeader.split(" ")[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Token tidak valid" });
+  }
+};
+
+export default verifyFirebaseToken;
